c23a8f6af771067686a9e7373e3955913460a941
GitHub Action <action@github.com>
2021-12-26 14:02:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/aakashtushar/intellijcejspplugin/MyBundle.kt
+++ src/main/kotlin/com/github/aakashtushar/intellijcejspplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aakashtushar.intellijcejspplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/aakashtushar/intellijcejspplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aakashtushar.intellijcejspplugin.services
+
+import com.github.aakashtushar.intellijcejspplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
